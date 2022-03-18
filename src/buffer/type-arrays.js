387fb9f8194d9c2f81671cdbe3fd1e5ea5b53f69
MatheusKael <matheuskael.silva@gmail.com>
2022-03-18 20:14:58 -0300

    Buffer Type arrays

---
+++ src/buffer/type-arrays.js
@@ -0,0 +1,8 @@
+const { Buffer } = require("buffer");
+
+const buf = Buffer.from([1, 2, 3, 4]);
+const uint32array = new Uint32Array(buf);
+
+console.log(uint32array);
+
+// Prints: Uint32Array(4) [ 1, 2, 3, 4 ]
