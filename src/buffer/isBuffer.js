457bd0d4d0153b538c19a6776ba2a66ede40e301
MatheusKael <matheuskael.silva@gmail.com>
2022-03-23 21:57:52 -0300

    is Buffer

---
+++ src/buffer/isBuffer.js
@@ -0,0 +1,7 @@
+const { Buffer } = require("buffer");
+
+Buffer.isBuffer(Buffer.alloc(10)); // true
+Buffer.isBuffer(Buffer.from("foo")); // true
+Buffer.isBuffer("a string"); // false
+Buffer.isBuffer([]); // false
+Buffer.isBuffer(new Uint8Array(1024)); // false
