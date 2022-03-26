bc7f4c5dbd78c15cf33d80b303bad404389cd150
MatheusKael <matheuskael.silva@gmail.com>
2022-03-26 20:48:35 -0300

    pool size

---
+++ src/buffer/poolsize.js
@@ -0,0 +1,15 @@
+const { Buffer } = require("buffer");
+
+// Copy an ASCII string into a `Buffer` one byte at a time.
+// (This only works for ASCII-only strings. In general, one should use
+// `Buffer.from()` to perform this conversion.)
+
+const str = "Node.js";
+const buf = Buffer.allocUnsafe(str.length);
+
+for (let i = 0; i < str.length; i++) {
+  buf[i] = str.charCodeAt(i);
+}
+
+console.log(buf.toString("utf8"));
+// Prints: Node.js
